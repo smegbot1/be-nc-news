28a0b870bbe6b18ac66b21e2c73f8eb6858a5930
Daniel Cruickshanks <dankom@live.de>
2019-11-26 12:24:13 +0000

    created test for status 404 invalid paths on any endpoint

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -10,6 +10,16 @@ after(() => client.destroy());
 beforeEach(() => client.seed.run());
 
 describe('/api', () => {
+    describe('INVALID PATH', () => {
+        it('Status: 404 returns error when an invalid path is entered on any endpoint', () => {
+            return request(app)
+                .get('/api/does-not-exist')
+                .expect(404)
+                .then(({ body: { msg } }) => {
+                    expect(msg).to.equal('Path not found, please enter an existing path.')
+                });
+        });
+    });
     describe('/topics', () => {
         describe('GET', () => {
             it('Status: 200 returns an array of topics', () => {
