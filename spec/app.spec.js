d6e0a87fe05c4c6dfb8479a6bf33f4aaa94bb6d5
Daniel Cruickshanks <dankom@live.de>
2019-11-26 12:09:25 +0000

    created status 200 test for GET/api/topics

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -4,4 +4,23 @@ const chai = require('chai');
 const { expect } = chai;
 const request = require('supertest');
 const app = require('../app');
-const client = require('../db');
+const client = require('../db')
+
+after(() => client.destroy());
+beforeEach(() => client.seed.run());
+
+describe('/api', () => {
+    describe('/topics', () => {
+        describe('GET', () => {
+            it('Status: 200 returns an array of topics', () => {
+                return request(app)
+                    .get('/api/topics')
+                    .expect(200)
+                    .then(({ body: { topics } }) => {
+                        expect(topics).to.be.an('array');
+                        expect(topics.length).to.equal(3);
+                    });
+            });
+        });
+    });
+});
