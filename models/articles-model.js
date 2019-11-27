b8a3014241a62e60bbe7bbcbdce3631cc616899c
Daniel Cruickshanks <dankom@live.de>
2019-11-27 22:03:30 +0000

    status: 200 GET/api/articles - passed author query test

--- models/articles-model.js
+++ models/articles-model.js
@@ -23,11 +23,12 @@ exports.updateArticle = (article_id, { inc_votes }) => {
         });
 };
 
-exports.fetchArticles = ({ sort_by, order }) => {
+exports.fetchArticles = ({ sort_by, order, author }) => {
     return client('articles')
         .select('articles.author', 'articles.title', 'articles.article_id', 'articles.topic', 'articles.created_at', 'articles.votes')
         .count({ comment_count: 'comment_id' })
         .leftJoin('comments', 'comments.article_id', 'articles.article_id')
         .groupBy('articles.article_id')
-        .orderBy(sort_by || 'created_at', order || 'desc');
+        .orderBy(sort_by || 'created_at', order || 'desc')
+        .modify(query => author ? query.where('articles.author', author) : query);
 };
--- query.sql
+++ query.sql
@@ -1,3 +1,3 @@
 \c nc_news_test
 
-SELECT * FROM comments WHERE article_id = 1;
+SELECT author FROM articles;
