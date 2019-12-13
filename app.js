4f77addbf3a07547999afeac283ed3853f796c19
Daniel Cruickshanks <dankom@live.de>
2019-12-13 14:31:16 +0000

    (feat): installed cors for cross-origin requests

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api-router');
 const { err404, err500, customErr, err400, err422 } = require('./errors');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 app.all('/*', err404);
